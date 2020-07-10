c3ca36c228398f27f37ad71b46bfeda661b872da
Fabio Pedro Ribeiro <si.fabioribeiro@gmail.com>
2020-07-10 14:31:05 -0300

    Cart is done test pass as well

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -39,23 +39,29 @@ const Cart: React.FC = () => {
   const { increment, decrement, products } = useCart();
 
   function handleIncrement(id: string): void {
-    // TODO
+    increment(id);
   }
 
   function handleDecrement(id: string): void {
-    // TODO
+    decrement(id);
   }
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((count, product) => {
+      const productSubtotal = product.price * product.quantity;
+      return count + productSubtotal;
+    }, 0);
 
-    return formatValue(0);
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((count, product) => {
+      const productQuantity = product.quantity;
+      return count + productQuantity;
+    }, 0);
 
-    return 0;
+    return total;
   }, [products]);
 
   return (
